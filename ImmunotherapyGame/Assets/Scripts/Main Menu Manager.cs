e2b06553e46d1ad712760a0fc8189bcdfd6fb8d9
GatoCreador887 <36546666+GatoCreador887@users.noreply.github.com>
2024-10-08 22:15:02 -0400

    Improve main menu with title, sprites, quit button

--- ImmunotherapyGame/Assets/Scripts/Main Menu Manager.cs
+++ ImmunotherapyGame/Assets/Scripts/Main Menu Manager.cs
@@ -9,4 +9,9 @@ public class MainMenuManager : MonoBehaviour
     {
         SceneManager.LoadScene("TutorialScene");
     }
+
+    public void QuitButtonClicked()
+    {
+        Application.Quit();
+    }
 }
