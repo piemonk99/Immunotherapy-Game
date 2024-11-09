0c3ced6fc5018a9b6b0762230ce9969054e3973d
GatoCreador887 <36546666+GatoCreador887@users.noreply.github.com>
2024-11-08 22:30:01 -0500

    Add some geometry for level 1 and some progress on setting up pathfinding for it

--- ImmunotherapyGame/Assets/Scripts/PathfindingNode.cs
+++ ImmunotherapyGame/Assets/Scripts/PathfindingNode.cs
@@ -1,3 +1,7 @@
+#if UNITY_EDITOR
+using System;
+using UnityEditor;
+#endif
 using UnityEngine;
 
 public class PathfindingNode : MonoBehaviour
@@ -8,4 +12,49 @@ public class PathfindingNode : MonoBehaviour
     {
         return connections;
     }
+
+    public void AddConnection(PathfindingNode other)
+    {
+        PathfindingNode[] newConnections = new PathfindingNode[connections.Length + 1];
+        Array.Copy(connections, newConnections, connections.Length);
+        newConnections[^1] = other;
+    }
 }
+
+#if UNITY_EDITOR
+// TODO replace with gizmos
+[CustomEditor(typeof(PathfindingNode))]
+public class PathfindingNodeEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        PathfindingNode node = (PathfindingNode)target;
+        Handles.color = new Color(1, 1, 0);
+        Handles.DrawSolidDisc(node.transform.position, Vector3.forward, 0.25f);
+
+        foreach (PathfindingNode connected in node.GetConnections())
+        {
+            Handles.color = new Color(1, 0.64f, 0);
+            Handles.DrawLine(node.transform.position, connected.transform.position);
+            Handles.color = new Color(0, 0.64f, 1);
+            Handles.DrawSolidDisc(connected.transform.position, Vector3.forward, 0.15f);
+        }
+
+        // Vector3 averagePosition = Vector3.zero;
+
+        // foreach (PathfindingNode node in targets)
+        // {
+        //     Handles.color = new Color(1, 1, 0);
+        //     Handles.DrawSolidDisc(node.transform.position, Vector3.forward, 0.25f);
+        //     averagePosition += node.transform.position;
+        // }
+
+        // averagePosition /= targets.Length;
+
+        // if (Handles.Button(averagePosition, Quaternion.identity, 0.5f, 1, Handles.CircleHandleCap))
+        //     foreach (PathfindingNode node in targets)
+        //         foreach (PathfindingNode node2 in targets)
+        //             node.AddConnection(node2);
+    }
+}
+#endif
